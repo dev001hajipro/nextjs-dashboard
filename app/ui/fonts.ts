c86e650c666ebfcfb3f843175b7e6ff6b2220698
dev001hajipro <dev001hajipro@gmail.com>
2024-12-12 20:56:25 +0900

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana, Noto_Sans_JP } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+export const noto_sans_jp = Noto_Sans_JP({
+    subsets: ["latin"],
+    weight: "400",
+});
